6e2d6c5d7523b85acdbea25d97ec849a6a476d54
Jairus Tan <jt3x2022@gmail.com>
2026-06-22 21:40:25 +0800

    Created global script and global position for playerr

--- enemies.gd
+++ enemies.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$RigidBody2D/AnimatedSprite2D.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$RigidBody2D/AnimatedSprite2D.play()
+	var player_pos = Vector2(0,0) #allows all enemies to know where player is
+	
+	
+	if Global.player: 
+		player_pos = Global.player.position
+		
