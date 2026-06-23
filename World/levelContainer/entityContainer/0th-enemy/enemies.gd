7f06d89acbda77aa14eb6292c2b0d8fbb9ab4e27
Jairus Tan <jt3x2022@gmail.com>
2026-06-23 16:09:20 +0800

    additional enemy changes to polish
    changelog
    
    - made enemy look at path instead of player so through walls dont look weird
    
    - added progress bar as health bar
    
    - added trigger distance for enemies so enemies can wait on the level

--- World/levelContainer/entityContainer/0th-enemy/enemies.gd
+++ World/levelContainer/entityContainer/0th-enemy/enemies.gd
@@ -4,6 +4,7 @@ var speed = 50
 var speed_max: float = 50 # entity should accelerate to this number
 var motion: Vector2 = Vector2(0, 0) # direction of velocity, unit vector
 var health = 100
+var trigger_distance = 600
 
 @onready var nav_agent = $NavigationAgent2D
 
@@ -12,29 +13,6 @@ func _ready() -> void:
 	$Area2D/AnimatedSprite2D.play("idle")
 	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#var player_pos: Vector2 #allows all enemies to know where player is
-	#
-	#
-	#if Global.player: 
-		#player_pos = Global.player.global_position
-		#
-	##move to player
-	#if player_pos:
-		#
-		#
-		#if global_position.x < player_pos.x - stopping_dist \
-		#or global_position.x > player_pos.x + stopping_dist \
-		#or global_position.y < player_pos.y - stopping_dist \
-		#or global_position.y > player_pos.y + stopping_dist:
-			#position = position.move_toward(player_pos, delta * speed)
-		#
-		#else: 
-			#print("contact")
-		#
-	#look_at(player_pos)		
 
 func _physics_process(delta: float) -> void:
 	var player_pos: Vector2 # player position
@@ -46,13 +24,14 @@ func _physics_process(delta: float) -> void:
 	if player_pos:
 		relatv_pos = global_position - player_pos # calc relative position
 		nav_agent.target_position = player_pos # pass player position as target for nav_agent
-		if relatv_pos.length() > stopping_dist: # analogous to the "is_target_reached" condition of NavAgent, more research required
+		if relatv_pos.length() > stopping_dist and relatv_pos.length() < trigger_distance: # analogous to the "is_target_reached" condition of NavAgent, more research required
 			if nav_agent.is_target_reachable():
 				#position = position.move_toward(nav_agent.get_next_path_position(), delta * speed)
 				nxtpat_pos = nav_agent.get_next_path_position()
 				nxtpat_pos = nxtpat_pos - global_position # shift frame
 				motion = nxtpat_pos.normalized()
 				position = position + speed*motion*delta
+				look_at(nav_agent.get_next_path_position())
 			else:
 				print("player is out of bounds! or perhaps not instantiated?")
 		else:
