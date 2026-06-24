7050514f3ce223f504f443d17fad5769179c4e3b
Jairus Tan <jt3x2022@gmail.com>
2026-06-24 23:04:41 +0800

    testing

--- World/levelContainer/entityContainer/Boss/boss.gd
+++ World/levelContainer/entityContainer/Boss/boss.gd
@@ -19,7 +19,6 @@ func _ready() -> void:
 
 
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	var player_pos = Global.player.global_position
