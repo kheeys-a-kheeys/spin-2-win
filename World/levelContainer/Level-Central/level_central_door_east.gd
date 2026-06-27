59be0e33402799a3a5132dcaea2d472f0ce9a002
Jairus Tan <jt3x2022@gmail.com>
2026-06-27 11:32:25 +0800

    tweaks to tutorial dialogue

--- World/levelContainer/Level-Central/level_central_door_east.gd
+++ World/levelContainer/Level-Central/level_central_door_east.gd
@@ -1,17 +1,20 @@
 extends Sprite2D
 
 var shut: bool = false # whether door is accessible or not, for encounters
-
+var tutorial_run_once = true
 # variable for when this door is called
 # REMEMBER: level has to be instantiated first BEFORE we move the player!
 @onready var exit_point: Vector2 = $"exit-point".global_position
 
 
 func _process(delta: float) -> void:
-	if $"../../CanvasLayer".tutorial_done == true:
-		shut = false
-	else:
-		shut = true
+	if tutorial_run_once:
+		if $"../../CanvasLayer".tutorial_done == true:
+			shut = false
+			tutorial_run_once = false
+		
+		else:
+			shut = true
 func _on_door_entered(area: Area2D) -> void:
 	if area.name == "player-shape":
 		if !shut:
